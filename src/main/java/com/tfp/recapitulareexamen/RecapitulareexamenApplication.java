50a06ae216f02cabe273a9a2b081cd42cf9855db
DESKTOP-91J784L\user <naum.cristi@yahoo.com>
2023-05-27 18:12:22 +0300

    first

---
+++ src/main/java/com/tfp/recapitulareexamen/RecapitulareexamenApplication.java
@@ -0,0 +1,13 @@
+package com.tfp.recapitulareexamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecapitulareexamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecapitulareexamenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tfp/recapitulareexamen/RecapitulareexamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfp.recapitulareexamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecapitulareexamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
